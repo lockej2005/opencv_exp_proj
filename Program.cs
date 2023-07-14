a0a5242593141742e7e973fdd91b32a525f1ba7e
lockej2005 <josh@intelligentinstallations.com.au>
2023-07-15 01:04:54 +1000

    first comittttt

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using OpenCvSharp;
+using System;
+
+namespace opencv_proj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Load the cascade
+            var faceCascade = new CascadeClassifier("haarcascade_frontalface_default.xml");
+
+            using (var capture = new VideoCapture(0))
+            using (var window = new Window("Camera"))
+            {
+                Mat frame = new Mat();
+                while (true)
+                {
+                    capture.Read(frame);
+
+                    if (!frame.Empty())
+                    {
+                        // Convert the frame to grayscale
+                        var gray = new Mat();
+                        Cv2.CvtColor(frame, gray, ColorConversionCodes.BGR2GRAY);
+
+                        // Detect faces
+                        var faces = faceCascade.DetectMultiScale(
+                            image: gray, 
+                            scaleFactor: 1.1, 
+                            minNeighbors: 5, 
+                            flags: HaarDetectionTypes.ScaleImage, 
+                            minSize: new OpenCvSharp.Size(30, 30)
+                        );
+
+                        // Draw a rectangle around the faces
+                        foreach (var face in faces)
+                        {
+                            Cv2.Rectangle(frame, face, new Scalar(0, 255, 0), thickness: 2);
+                        }
+                    }
+
+                    Cv2.ImShow("Camera", frame);
+
+                    if (Cv2.WaitKey(1) >= 0)
+                        break;
+                }
+            }
+        }
+    }
+}
